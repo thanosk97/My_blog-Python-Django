4f6c67c1f11483ee9994ae86d2c66aad35b7f317
thanosk97 <thanosk97@gmail.com>
2024-11-27 14:45:22 +0200

    Models & Admin

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Post, Author, Tag
+
+#thanos
+#1997
+
+
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
